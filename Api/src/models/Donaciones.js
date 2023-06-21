3f2eb8385b74c2413b30b58979887ee54e1d833d
Dalejandro31 <74673610+Dalejandro31@users.noreply.github.com>
2023-06-21 16:48:05 -0500

    Modelos

---
+++ Api/src/models/Donaciones.js
@@ -0,0 +1,27 @@
+const {DataTypes, } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('donacion', {
+        id:{
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey:true,
+            allowNull: false
+        },
+        monto: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false
+        },
+        fecha: {
+            type: DataTypes.DATEONLY,
+            allowNull: false
+        },
+        descripcion: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+    },
+    {
+        timestamps: false,     
+    });
+} 
