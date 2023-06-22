8e50389d108348353d8037484cf142a6d858bd17
ramiandino <rami.andino32@gmail.com>
2023-06-22 18:23:55 -0300

    card

--- Client/src/componentes/Cartas/cards.jsx
+++ Client/src/componentes/Cartas/cards.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function Card ({img,name,genres,edad,id}) {  
     return (
         <div>
-            <img src={img} alt="imagen card" />
+            <img src={img} alt="imagen card"/>
              <div>{name[0].toUpperCase() + name.slice(1)}</div>
             <div>
                 <p>{genres}</p>
