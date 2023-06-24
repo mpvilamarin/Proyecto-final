67f6e65c785b7e0b5624921a6815d7a710e17f1e
mpvilamarin <mpvillamarin@gmail.com>
2023-06-24 17:05:34 -0300

    fundaciones y paginación

--- Client/src/app.js
+++ Client/src/app.js
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './componentes/Home/home'
 import Detalle from './componentes/Detalle/detalle';
 import Nosotros from './componentes/Nosotros/nosotros';
+import Adopcion from './componentes/Adopción/adopcion';
+import NavBar from './componentes/NavBar/navbar';
 
 
 // const location = useLocation();
@@ -10,13 +12,13 @@ import Nosotros from './componentes/Nosotros/nosotros';
 function App() {
   return (
       <div>
+        <NavBar />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/detalle" element={<Detalle />} />
-            <Route path="/nosotros" element={<Nosotros />} />
-
+            <Route path="/about" element={<Nosotros />} />
             <Route path="/detalle/:id" element={<Detalle />} />
-
+            <Route path="/adopciones" element={<Adopcion />} />
            
           </Routes>
       </div>
