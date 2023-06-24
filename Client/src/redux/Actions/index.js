51d2b4233c46a64d29151a3e76337b33fe44d004
SantiMarich <santiagomarich@gmail.com>
2023-06-23 21:49:41 -0300

    FundacionDetail

--- Client/src/redux/Actions/index.js
+++ Client/src/redux/Actions/index.js
@@ -1,29 +1,66 @@
-import axios from 'axios'
-import{
-    GET_ALL_MASCOTAS,
-    GET_DETAIL_MASCOTAS
-} from '../Actions-type/index.js';
+import axios from "axios";
+import {
+  GET_ALL_MASCOTAS,
+  GET_DETAIL_MASCOTAS,
+  GET_ALL_FUNDACIONES,
+  GET_DETAIL_FUNDACION,
+  RESET_DETAIL,
+} from "../Actions-type/index.js";
 
 //Actions Mascotas ------->>>
 
-
 export const getAllMascotas = () => {
-    return async(dispatch) => {
-        await axios.get('http://localhost:3001/mascotas')
-        .then((res) => res.data)
-        .then((data) => {
-            console.log('payload:', data)
-            dispatch({type: GET_ALL_MASCOTAS, payload: data});
-        })
-    }
-}
+  return async (dispatch) => {
+    await axios
+      .get("http://localhost:3001/mascotas")
+      .then((res) => res.data)
+      .then((data) => {
+        console.log("payload:", data);
+        dispatch({ type: GET_ALL_MASCOTAS, payload: data });
+      });
+  };
+};
 
 export const getDetailMascota = (id) => {
-    return async(dispatch) => {
-        dispatch({type: GET_DETAIL_MASCOTAS, payload: []});
-        await axios.get(`http://localhost:3001/mascotas/${id}`)
-        .then((res) => res.data)
-        .then((data) => dispatch({type: GET_DETAIL_MASCOTAS, payload: data}))
-        .catch((err) => console.log(err))
+  return async (dispatch) => {
+    dispatch({ type: GET_DETAIL_MASCOTAS, payload: [] });
+    await axios
+      .get(`http://localhost:3001/mascotas/${id}`)
+      .then((res) => res.data)
+      .then((data) => dispatch({ type: GET_DETAIL_MASCOTAS, payload: data }))
+      .catch((err) => console.log(err));
+  };
+};
+
+export const getAllFundacioness = () => {
+  return async (dispatch) => {
+    await axios
+      .get("http://localhost:3001/fundaciones")
+      .then((res) => res.data)
+      .then((data) => {
+        console.log("payload:", data);
+        dispatch({ type: GET_ALL_FUNDACIONES, payload: data });
+      });
+  };
+};
+
+export const getDetailFundacion = (id) => {
+  return async (dispatch) => {
+    dispatch({ type: GET_DETAIL_FUNDACION, payload: [] });
+    await axios
+      .get(`http://localhost:3001/fundaciones/${id}`)
+      .then((res) => res.data)
+      .then((data) => dispatch({ type: GET_DETAIL_FUNDACION, payload: data }))
+      .catch((err) => console.log(err));
+  };
+};
+
+export const resetDetail = () => {
+  return async function (dispatch) {
+    try {
+      dispatch({ type: RESET_DETAIL });
+    } catch (error) {
+      return { error: error.message };
     }
-}
+  };
+};
--- Client/src/redux/reducer/index.js
+++ Client/src/redux/reducer/index.js
@@ -1,47 +1,70 @@
-import{
-    GET_ALL_MASCOTAS,
-    GET_DETAIL_MASCOTAS
-} from '../Actions-type/index.js';
+import {
+  GET_ALL_MASCOTAS,
+  GET_DETAIL_MASCOTAS,
+  GET_ORDER_AZ,
+  GET_ORDER_ZA,
+  GET_ALL_FUNDACIONES,
+  GET_DETAIL_FUNDACION,
+  RESET_DETAIL,
+} from "../Actions-type/index.js";
 
 const initialState = {
-    mascotas:[],
-    filtroMascotas: [],
-    mascotaDetail: [],
+  mascotas: [],
+  filtroMascotas: [],
+  mascotaDetail: [],
+  fundaciones: [],
+  fundacionDetail: [],
+  selectedFundacion: null,
+};
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_ALL_MASCOTAS:
+      return {
+        ...state,
+        mascotas: action.payload,
+        filtroMascotas: action.payload,
+      };
+    case GET_DETAIL_MASCOTAS:
+      return {
+        ...state,
+        mascotaDetail: {
+          ...action.payload,
+          mascotas: action.payload.mascotas || [],
+        },
+      };
+    case GET_ALL_FUNDACIONES:
+      return {
+        ...state,
+        fundaciones: action.payload,
+      };
+    case GET_DETAIL_FUNDACION:
+      return {
+        ...state,
+        fundacionDetail: {
+          ...action.payload,
+          fundaciones: action.payload.fundaciones || [],
+        },
+      };
+    case GET_ORDER_AZ:
+      return {
+        ...state,
+        mascotas: state.mascotas
+          .slice()
+          .sort((a, b) => a.nombre.localeCompare(b.nombre)),
+      };
+    case GET_ORDER_ZA:
+      return {
+        ...state,
+        mascotas: state.mascotas
+          .slice()
+          .sort((a, b) => b.nombre.localeCompare(a.nombre)),
+      };
+    case RESET_DETAIL:
+      return { ...state, selectedFundacion: null };
+
+    default:
+      return state;
+  }
 }
-function rootReducer (state = initialState, action)
-{
-    switch (action.type) {
-        case GET_ALL_MASCOTAS:
-            return {
-                ...state,
-                mascotas: action.payload,
-                filtroMascotas: action.payload
-            };
-        case GET_DETAIL_MASCOTAS:
-            return{
-                ...state,
-                mascotaDetail: {
-                    ...action.payload,
-                    mascotas: action.payload.mascotas || []
-                }
-            };
-        case GET_ORDER_AZ:
-            return{
-                ...state,
-                mascotas: state.mascotas.slice().sort((a,b) => a.nombre.localeCompare(b.nombre))
-            }
-        case GET_ORDER_ZA:
-            return{
-                ...state,
-                mascotas: state.mascotas.slice().sort((a,b) => b.nombre.localeCompare(a.nombre))
-            }            
-        default:
-            return state;
-
-    }
-
-}
-
-
 
 export default rootReducer;
