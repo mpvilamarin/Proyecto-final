c6d50c291dd58c73b67960323c003b5bc9314015
Dalejandro31 <74673610+Dalejandro31@users.noreply.github.com>
2023-06-24 19:33:31 -0500

    formulario y comodin en la rutas

--- Client/src/app.js
+++ Client/src/app.js
@@ -5,6 +5,7 @@ import Detalle from './componentes/Detalle/detalle';
 import Nosotros from './componentes/Nosotros/nosotros';
 import Adopcion from './componentes/Adopción/adopcion';
 import NavBar from './componentes/NavBar/navbar';
+import FormFundaciones from './componentes/FormFundaciones/FormFundaciones.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -21,7 +22,9 @@ function App() {
             <Route path="/about" element={<Nosotros />} />
             <Route path="/detalle/:id" element={<Detalle />} />
             <Route path="/adopciones" element={<Adopcion />} />
-           
+            <Route path="/formFundaciones" element={<FormFundaciones/>}/>
+            {/* <Route path='*' element={<Navigate to='/error'/>}/>
+            <Route path="/error" element={< Redirect/>} /> */}
           </Routes>
       </div>
   );
