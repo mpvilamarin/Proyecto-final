2d7cbf21eebd71895af78c19184bdc499f4150db
Dalejandro31 <74673610+Dalejandro31@users.noreply.github.com>
2023-06-25 17:23:46 -0500

    formularioDeMascotas

--- Client/src/componentes/Forms/FormMascota.jsx
+++ Client/src/componentes/Forms/FormMascota.jsx
@@ -1,12 +1,18 @@
-import React, {useState} from 'react'; 
+import React, {useState, useEffect} from 'react'; 
 import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { postMascota } from '../../redux/Actions/post';
+import { getAllFundaciones } from '../../redux/Actions/get'
 
 function FormFundaciones(){ 
 
-    const mascotas = useSelector( state => state.mascotas);
+    const fundaciones = useSelector( state => state.fundaciones);
     const dispatch = useDispatch();
+    
+    useEffect(() => {
+        dispatch(getAllFundaciones())
+    },[dispatch])
+
 
     const [newMascota, setNewMascota] = useState({
         nombre: '',
@@ -17,6 +23,7 @@ function FormFundaciones(){
         temperamento: '',
         descripcion: '',
         castrado: '',
+        fundacionId: []
     })
  
     const handleChange = (e) => {
@@ -24,6 +31,20 @@ function FormFundaciones(){
             ...newMascota,
             [e.target.name] : e.target.value,
         })
+    }
+
+    const handleChecked = (e) =>{
+        if( e.target.checked){
+            setNewMascota({
+                ...newMascota,
+                fundacionId: [...newMascota.fundacionId, e.target.value ]
+            })
+        }else{
+            setNewMascota({
+                ...newMascota,
+                fundacionId: newMascota.fundacionId.filter((fundacion) => fundacion !== e.target.value)
+            })
+        }
     }
 
 
@@ -40,8 +61,15 @@ function FormFundaciones(){
             temperamento: '',
             descripcion:'',
             castrado: '',
+            fundacionId: []
         })
     }
+
+
+    const sortedFundacion = fundaciones.slice().sort((a,b) => {
+        return a.nombre.localCompare(b.nombre);
+    })
+
     return( 
     <Form> 
         <Form.Group controlId="formBasicNombre"> <Form.Label>Nombre</Form.Label> <Form.Control type="text" name="nombre" value={newMascota.nombre} onChange={handleChange} placeholder="Nombre de la mascota" /> </Form.Group>
@@ -52,6 +80,24 @@ function FormFundaciones(){
         <Form.Group controlId="formBasicTemperamento"> <Form.Label>Temperamento</Form.Label> <Form.Control type="text" name="temperamento" value={newMascota.temperamento} placeholder="Temperamento" onChange={handleChange} /> </Form.Group>
         <Form.Group controlId="formBasicDescripcion"> <Form.Label>Descripcion</Form.Label> <Form.Control type="text" name="descripcion" value={newMascota.descripcion} placeholder="Descripcion" onChange={handleChange} /> </Form.Group>
         <Form.Group controlId="formBasicCastrado"> <Form.Label>Castrado</Form.Label> <Form.Control type="text" name="castrado" value={newMascota.castrado} onChange={handleChange} placeholder="Castrado" /> </Form.Group>
+        
+        <div >
+            <div>
+                {
+                    sortedFundacion.length >= 1 ?
+                        sortedFundacion?.map((elem, index) => (
+                        <div key={index}>
+                            <label htmlFor={`fundacionId_${index}`} key={index}>
+                                <input type="checkbox" name={`fundacionId_${index}`} value={elem} key={index} onChange={handleChecked}/>
+                                {elem.nombre}
+                            </label>
+                        </div>
+                    ))
+                    :undefined
+                }
+            </div>
+        </div>
+        
         <Button variant="primary" type="submit" onClick={(e) => handleSubmit(e)}> Submit </Button>
                
         
