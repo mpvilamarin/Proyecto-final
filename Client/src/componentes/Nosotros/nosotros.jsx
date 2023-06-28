90f0d489a27f59cdec6f412ec5e9e94f9386e3b4
mpvilamarin <mpvillamarin@gmail.com>
2023-06-28 14:36:16 -0300

    nosotros

--- Client/src/componentes/Nosotros/nosotros.jsx
+++ Client/src/componentes/Nosotros/nosotros.jsx
@@ -1,57 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactCardFlip from "react-card-flip";
+import styles from "./nosotros.module.css";
 
 export default function Nosotros() {
+
+    const [flip, setFlip] = useState(false);
+
+    const handleMouseEnter = () => {
+        setFlip(true);
+    };
+
+    const handleMouseLeave = () => {
+        setFlip(false);
+    };
+
     return (
         <div className="container">
-            <h2>Nuestro Equipo</h2>
+            <h2 className={styles.title} >Nuestro Equipo</h2>
 
             <h3>Back-End</h3>
-            <div className="developer-container">
-                <div>
-                    <img src="" alt="" />
-                    <p>David</p>
-                    <p>Hola, soy David, blablabla</p>
+            <div className={styles.backContainer}>
+
+                <ReactCardFlip isFlipped={flip} flipDirection="horizontal">
+                    <div className={styles.content} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                        <div>
+                            <img className={styles.image} src={require("../../assets/David.png")} alt="David" />
+                            <p className={styles.name}>David</p>
+                        </div>
+                    </div>
+                    <div className={styles.content} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                        <p className={styles.info}>Hola, soy estudiante Full Stack Developer en HENRY.</p>
+                    </div>
+                </ReactCardFlip>
+
+                <div className={styles.content}>
+                    <img className={styles.image} src={require("../../assets/Nahuel.png")} alt="Nahuel" />
+                    <p className={styles.name} >Nahuel</p>
+                    <p className={styles.info} >Hola, soy Nahuel, blablabla</p>
+
                 </div>
-                <div>
-                    <img src="" alt="" />
-                    <p>Nahuel</p>
-                    <p>Hola, soy Nahuel, blablabla</p>
-                </div>
-                <div>
-                    <img src="" alt="" />
-                    <p>Jonny</p>
-                    <p>Hola, soy Jonny, blablabla</p>
-                </div>
-                <div>
-                    <img src="" alt="" />
-                    <p>Santi</p>
-                    <p>Hola, soy Santi, blablabla</p>
+
+                <div className={styles.content}>
+                    <div className={styles.imageContainer}>
+                        <img className={styles.image} src={require("../../assets/Santi.png")} alt="Santi" />
+                        <p className={styles.name} >Santi</p>
+                        <p className={styles.info} >Hola, soy Santi, blablabla</p>
+                    </div>
+
                 </div>
             </div>
 
-            <h3>Front-End</h3>
-            <div className="developer-container">
-            <div>
-                    <img src="" alt="" />
-                    <p>María</p>
-                    <p>Hola, soy María, blablabla</p>
+            <h3 className={styles.sub} >Front-End</h3>
+            <div className={styles.frontContainer}>
+
+
+                <div className={styles.content}>
+                    <img className={styles.image} src={require("../../assets/Mari.png")} alt="Mari" />
+                    <p className={styles.name} >María</p>
+                    <p className={styles.info} >Hola, soy María, blablabla</p>
+
                 </div>
-                <div>
-                    <img src="" alt="" />
-                    <p>Juan</p>
-                    <p>Hola, soy Juan, blablabla</p>
+                <div className={styles.devCard}>
+                    <div className={styles.content}>
+                        <img className={styles.image} src={require("../../assets/Juan.png")} alt="Juan" />
+                        <p className={styles.name} >Juan</p>
+                        <p className={styles.info} >Hola, soy Juan, blablabla</p>
+                    </div>
                 </div>
-                <div>
-                    <img src="" alt="" />
-                    <p>Rami</p>
-                    <p>Hola, soy Rami, blablabla</p>
+
+                <div className={styles.content}>
+                    <img className={styles.image} src={require("../../assets/Rami.png")} alt="Rami" />
+                    <p className={styles.name} >Rami</p>
+                    <p className={styles.info} >Hola, soy Rami, blablabla</p>
+
                 </div>
-                <div>
-                    <img src="" alt="" />
-                    <p>Paula</p>
-                    <p>Hola, soy Paula, blablabla</p>
+
+                <div className={styles.content}>
+                    <img className={styles.image} src={require("../../assets/Paula.png")} alt="Paula" />
+                    <p className={styles.name} >Paula</p>
+                    <p className={styles.info} >Hola, soy Paula, blablabla</p>
                 </div>
+
             </div>
         </div>
     );
-};
+}
