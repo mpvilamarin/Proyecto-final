26b1560b0be26b892d1e3186b706a7ea8521ed81
Dalejandro31 <74673610+Dalejandro31@users.noreply.github.com>
2023-06-28 16:07:21 -0500

    logueo para usuario y fundacion

--- Api/src/routes/login.js
+++ Api/src/routes/login.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const loginRouter = express.Router();
-const { postRegistroUsuario, getRegistroUsuario, updateUsuario, loginUsuario ,deleteUsuario, getIdUsuario } = require('../Handlers/UsuarioHandler');
+const { postRegistroUsuario, getRegistroUsuario, updateUsuario, deleteUsuario, getIdUsuario } = require('../Handlers/UsuarioHandler');
+const {loginUsuarios} = require('../Handlers/LoginHandler')
 
 loginRouter.post('/', postRegistroUsuario);
-loginRouter.post('/login', loginUsuario);
+loginRouter.post('/login', loginUsuarios);
 loginRouter.get('/',getRegistroUsuario);
 loginRouter.get('/:id', getIdUsuario);  
 loginRouter.put('/:email',updateUsuario);
