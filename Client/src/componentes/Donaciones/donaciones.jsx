9f15b9c862b9c1e786fb0ab665d9df2c91944e4a
SantiMarich <santiagomarich@gmail.com>
2023-07-01 00:56:44 -0300

    AvancePasarelaPago

--- Client/src/componentes/Donaciones/donaciones.jsx
+++ Client/src/componentes/Donaciones/donaciones.jsx
@@ -1,67 +1,66 @@
-import axios from 'axios';
-import React, { useState } from "react"
-// import { useParams } from "react-router-dom";
-import style from './Donaciones.module.css'
-import { initMercadoPago, Wallet } from '@mercadopago/sdk-react'
-import logoMercadoPago from './mercadopago.png';
+import axios from "axios";
+import React, { useState } from "react";
+import logoMercadoPago from "./mercadopago.png";
 
-export default function Donacion() {
-
-  // const { fundacionId } = useParams();
-  const [inputValue, setInputValue] = useState('');
-  const [preferenceId, setPreferenceId] = useState(null);
-  initMercadoPago('TEST-02a2559f-5988-4feb-9e00-0b05cc1e1ab3');
+const Donaciones = () => {
+  const [donacion, setDonacion] = useState({
+    title: "",
+    price: 0,
+    quantity: 0,
+  });
 
   const handleInputChange = (e) => {
-    setInputValue(e.target.value);
+    setDonacion({
+      ...donacion,
+      [e.target.name]: e.target.value,
+    });
   };
 
-  // mercadopago functions
-  const createPreference = async () => {
-    try {
-      const response = await axios.post("http://localhost:3001/create_preference", {
-        description: "Gracias por donar",
-        price: inputValue,
-        quantity: 1
+  const handleDonarClick = () => {
+    axios
+      .post("http://localhost:3001/pasarelaPago/donacion", donacion)
+      .then((res) => (window.location.href = res.data.response.body.init_point))
+      .catch((error) => {
+        console.log(error);
       });
-      const { id } = response.data;
-      return id;
-    } catch (error) {
-      console.log(error.message)
-    }
   };
-  const handleBuy = async () => {
-    const id = await createPreference();
-    if (id) {
-      setPreferenceId(id);
-    }
-  };
+
   return (
-    <div className={style.container}>
-      <div><h1>Donar es darles una oportunidad de ser amados y cuidados</h1></div>
-      <div className={style.inputContainer}>
+    <div>
+      <h1>Donar es darles una oportunidad de ser amados y cuidados</h1>
+      <form>
+        <input
+          type="text"
+          name="title"
+          value={donacion.title}
+          onChange={handleInputChange}
+          placeholder="Descripcion"
+        />
         <input
           type="number"
-          className={style.donateInput}
           min="10"
-          value={inputValue}
+          name="price"
+          value={donacion.price}
           onChange={handleInputChange}
+          placeholder="Monto"
         />
-        <div className={style.p}>
-          <p className={style.donateAmount}>Le donaras a los peluditos: ${inputValue} </p>
-        </div>
-      </div>
-      <div className={style.inputContainer}>
-        <button className={style.buttonDonar} onClick={handleBuy}>
+        <input
+          type="number"
+          min="1"
+          name="quantity"
+          value={donacion.quantity}
+          onChange={handleInputChange}
+          placeholder="Cantidad"
+        />
+        <button type="button" onClick={handleDonarClick}>
           Donar
         </button>
-        {
-          preferenceId && <Wallet initialization={{ preferenceId: preferenceId }} />
-        }
-      </div>
-      <div className={style.mercadoPagoLogo}>
+      </form>
+      <div>
         <img src={logoMercadoPago} alt="MercadoPago Logo" />
       </div>
     </div>
-  )
-}
+  );
+};
+
+export default Donaciones;
