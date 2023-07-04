eed8d6dca767f40c3bd85077b7c301d1e5505aab
SantiMarich <santiagomarich@gmail.com>
2023-07-04 18:45:56 -0300

    IdUsuario+BorrarMascota

--- Client/src/componentes/Cartas/cardMascotas.jsx
+++ Client/src/componentes/Cartas/cardMascotas.jsx
@@ -1,82 +1,35 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteMascota } from "../../redux/Actions/delete";
 import { getAllMascotas } from "../../redux/Actions/get";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import "./cards.css";
-import mascotas from "./perroGato.png";
 
 export default function CardMascota() {
   const dispatch = useDispatch();
   const allMascotas = useSelector((state) => state.mascotas);
-  console.log(allMascotas);
+
   useEffect(() => {
     dispatch(getAllMascotas());
   }, [dispatch]);
 
+  const handleAdoptar = (nombre) => {
+    dispatch(deleteMascota(nombre));
+  };
+
   return (
-    <div>
-      <div className="title-container">
-        <h1 className="card-title-highlight">MASCOTAS</h1>
-      </div>
-      {allMascotas && allMascotas.length > 0 ? (
-        <Carousel
-          showThumbs={false}
-          showArrows={true}
-          showStatus={false}
-          centerMode={true}
-          centerSlidePercentage={33.33}
-          infiniteLoop={false} // Cambio realizado aquí
-          emulateTouch={true}
-          renderArrowPrev={(onClickHandler, hasPrev, label) =>
-            hasPrev && (
-              <button
-                type="button"
-                className="control-arrow control-prev"
-                onClick={onClickHandler}
-                title={label}
-              />
-            )
-          }
-          renderArrowNext={(onClickHandler, hasNext, label) =>
-            hasNext && (
-              <button
-                type="button"
-                className="control-arrow control-next"
-                onClick={onClickHandler}
-                title={label}
-              />
-            )
-          }
-        >
-          {allMascotas.map((mascota, indexMascota) => (
-            <div key={indexMascota} className="card-carousel">
-              <div className="image-container">
-                <Link to={`/mascota/${mascota.id}`}>
-                  <img
-                    src={mascotas}
-                    alt="Mascota"
-                    className="card-image"
-                  />
-                </Link>
-              </div>
-              <div className="card-caption">
-                <Link to={`/mascota/${mascota.id}`}>
-                  <h2 className="card-title">{mascota.nombre}</h2>
-                </Link>
-                <div className="card-info">
-                  <h4>Género: {mascota.genero}</h4>
-                  <h4>Temperamento: {mascota.temperamento}</h4>
-                </div>
-              </div>
-            </div>
-          ))}
-        </Carousel>
-      ) : (
-        <h3>No hay mascotas disponibles.</h3>
-      )}
+    <div className="card-container">
+      {allMascotas.map((mascota, index) => (
+        <div key={index} className="card">
+          <img src={mascota.imagen} alt={mascota.nombre} />
+          <h2>{mascota.nombre}</h2>
+          <p>{mascota.descripcion}</p>
+          <button
+            onClick={() => handleAdoptar(mascota.nombre)}
+            disabled={mascota.adoptado}
+          >
+            {mascota.adoptado ? "Adoptado" : "Adoptar"}
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
-
