4032eba8b49471e5eb0d54ecfa248b8c59c29f8f
Maria Salazar <mvsalazarflores@gmail.com>
2023-07-06 00:41:02 -0300

    cambioss

--- Api/src/db.js
+++ Api/src/db.js
@@ -5,17 +5,15 @@ const fs = require('fs');
 const path = require('path');
 const { DB_DEPLOY } = process.env;
 
+const {
+  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME
+} = process.env;
+
 // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, 
-const sequelize = new Sequelize(DB_DEPLOY, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialectOptions : {
-    ssl: {
-      require: true,
-    }
-
-  }
-);
+});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
