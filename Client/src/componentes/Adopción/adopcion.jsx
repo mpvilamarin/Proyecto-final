9c0610e23085edf99fddb0edfe44eeadfb120143
mpvilamarin <mpvillamarin@gmail.com>
2023-07-10 16:28:29 -0300

    estilos

--- Client/src/componentes/Adopción/adopcion.jsx
+++ Client/src/componentes/Adopción/adopcion.jsx
@@ -27,7 +27,7 @@ const Adopcion = () => {
   const [generoFilter, setGeneroFilter] = useState("");
   const [ordenamiento, setOrdenamiento] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const [elementsPerPage] = useState(4);
+  const [elementsPerPage] = useState(8);
   const [especie, setEspecie] = useState("");
   const petsFilter = useSelector((state) => state.mascotas);
 
@@ -98,54 +98,79 @@ const Adopcion = () => {
           />
         ))}
       </div> */}
-      <div className={styles.containerFiltros}>
-        <h1>Elige las características de tu mascota</h1>
-        <div className={styles.selectoresWrapper}>
-          <div className={styles.divSelector}>
-            <label>Género:</label>
-            <select
-              value={generoFilter}
-              onChange={handleGeneroFilter}
-              className={styles.options}
-            >
-              <option value="">Todos</option>
-              <option value="Macho">Macho</option>
-              <option value="Hembra">Hembra</option>
-              <option value="Desconocido">Desconocido</option>
-            </select>
-          </div>
-          <div>
-            <label>Ordenar por nombre:</label>
-            <button onClick={handleSortAsc}>A-Z</button>
-            <button onClick={handleSortDesc}>Z-A</button>
-          </div>
-          <div>
-            <label htmlFor="especie">Especie:</label>
-            <select
-              id="especie"
-              value={especie}
-              onChange={handleEspecieFilter}
-              className={styles.options}
-            >
-              <option value="">Perros y gatos</option>
-              <option value="Perro">Perros</option>
-              <option value="Gato">Gatos</option>
-            </select>
-          </div>
-          <div>
-            <label htlmFor="fundacion">Fundación:</label>
-            <select
-              className={styles.options}
-              onChange={handleFundacion}
-              value={selectedFundacion}
-            >
-              <option value="All">Ver todas las fundaciones</option>
-              {uniqueFundaciones.map((x, index) => (
-                <option value={x} key={index}>
-                  {x}
-                </option>
-              ))}
-            </select>
+      <div className={styles.caja}>
+        <div>
+          <img
+            src={require("../../assets/MascotaFundacion2.png")}
+            alt="mascotas"
+            className={styles.img}>
+          </img>
+        </div>
+        <div className={styles.containerFiltros}>
+          <h1>Mascotas en adopción</h1>
+          <h2 className={styles.sub}>Busca tu mascota ideal</h2>
+
+          <div className={styles.cont1}>
+            <div className={styles.selectoresWrapper}>
+              <div className={styles.divSelector}>
+                <label className={styles.marca}>Género:</label>
+                <select
+                  value={generoFilter}
+                  onChange={handleGeneroFilter}
+                  className={styles.options}
+                >
+                  <option value="">Todos</option>
+                  <option value="Macho">Macho</option>
+                  <option value="Hembra">Hembra</option>
+                  <option value="Desconocido">Desconocido</option>
+                </select>
+              </div>
+              <div className={styles.radioOption}>
+                <label className={styles.marca}>Especie:</label>
+                <div className={styles.radioOption}>
+                  <input type="radio" id="ambos" name="especie" value="" checked={especie === ""} onChange={handleEspecieFilter} />
+                  <label htmlFor="ambos">
+                    <img src={require("../../assets/AmbosFiltro.png")} alt="Ambos" className={styles.radioImg2} />
+                  </label>
+                </div>
+                <div className={styles.radioOption}>
+                  <input type="radio" id="perro" name="especie" value="Perro" checked={especie === "Perro"} onChange={handleEspecieFilter} />
+                  <label htmlFor="perro">
+                    <img src={require("../../assets/PerroFiltro.png")} alt="Perro" className={styles.radioImg} />
+                  </label>
+                </div>
+                <div className={styles.radioOption}>
+                  <input type="radio" id="gato" name="especie" value="Gato" checked={especie === "Gato"} onChange={handleEspecieFilter} />
+                  <label htmlFor="gato">
+                    <img src={require("../../assets/GatoFiltro.png")} alt="Gato" className={styles.radioImg} />
+                  </label>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.cont2}>
+              <div>
+                <label className={styles.marca}>Ordenar por nombre:</label>
+                <button onClick={handleSortAsc} className={styles.aZ}>A-Z</button>
+                <button onClick={handleSortDesc} className={styles.aZ}>Z-A</button>
+              </div>
+              <div>
+                <label className={styles.marca}>Fundación:</label>
+                <select
+                  className={styles.options}
+                  onChange={handleFundacion}
+                  value={selectedFundacion}
+                >
+                  <option value="All">Ver todas las fundaciones</option>
+                  {uniqueFundaciones.map((x, index) => (
+                    <option value={x} key={index}>
+                      {x}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
@@ -164,7 +189,7 @@ const Adopcion = () => {
           <div>
             {currentPage > 1 && (
               <button className={styles.button} onClick={paginationButtonPrev}>
-                Prev
+                <img src={require("../../assets/Prev.png")} alt="Next" className={styles.buttonImg} />
               </button>
             )}
           </div>
@@ -179,7 +204,7 @@ const Adopcion = () => {
           <div>
             {Math.ceil(allPets.length / elementsPerPage) > currentPage && (
               <button className={styles.button} onClick={paginationButtonNext}>
-                Next
+                <img src={require("../../assets/Next.png")} alt="Next" className={styles.buttonImg} />
               </button>
             )}
           </div>
