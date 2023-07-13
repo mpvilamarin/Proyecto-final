a55216810f6eb80ad512486cf594889e0cfaa61d
mpvilamarin <mpvillamarin@gmail.com>
2023-07-13 13:56:11 -0300

    donaciones

--- Client/src/componentes/DashboardAdmin/Donaciones/Donaciones.jsx
+++ Client/src/componentes/DashboardAdmin/Donaciones/Donaciones.jsx
@@ -17,23 +17,16 @@ const DonacionesHechas = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Donaciones Hechas</h1>
-      <div className={styles.container}>
-        <div className={styles.aprobada}>
-          <h2 className={styles.sub}>Transacciones Aprobadas</h2>
-        </div>
-        <div className={styles.rechazada}>
-          <h2 className={styles.sub}>Transacciones Rechazadas</h2>
-          {donacionHecha.map((donacion) => (
-            <div key={donacion.id}>
-              <p className={styles.content}>ID: {donacion.id}</p>
-              <p className={styles.content}>Monto: {donacion.monto}</p>
-              <p className={styles.content}>Fecha: {donacion.fecha}</p>
-              <p className={styles.contentFinal}>Descripción: {donacion.descripcion}</p>
-            </div>
-          ))}
-        </div>
+      <div className={styles.contTrans}>
+        {donacionHecha.map((donacion) => (
+          <div key={donacion.id}>
+            <p className={styles.content}>ID: {donacion.id}</p>
+            <p className={styles.content}>Monto: {donacion.monto}</p>
+            <p className={styles.content}>Fecha: {donacion.fecha}</p>
+            <p className={styles.contentFinal}>Descripción: {donacion.descripcion}</p>
+          </div>
+        ))}
       </div>
-
     </div>
   );
 };
