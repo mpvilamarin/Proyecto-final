cef90e92f45ea1377d6fededa86e3d9cef285a92
Nahuel Castilla <nahuelcastilla22@gmail.com>
2023-07-15 16:24:32 -0300

    tercera chance

--- Api/app.js
+++ Api/app.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 require('dotenv').config();
 const { PORT } = process.env;
 
-conn.sync({ force: true}).then(() => {
+conn.sync({ force: true}).then(() => { 
 
   server.listen(PORT || 3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
